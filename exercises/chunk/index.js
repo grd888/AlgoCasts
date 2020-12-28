37b0746c78447d39c050ba44cdb69e628c3c2204
Greg Delgado <gregd@towa.network>
2020-12-29 06:24:57 +0800

    chunked array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,26 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1
+function chunk(array, size) {
+  let result = [];
+  for (let i = 0; i < array.length; i += size) {
+    const slice = array.slice(i, i + size);
+    result.push(slice);
+  }
+  return result;
+}
 
+// Solution 2
+// function chunk(array, size) {
+//   let result = [];
+//   while(array.length > 0) {
+//       const slice = [];
+//       for(let i = 0; i < size; i++) {
+//         if(array.length > 0) slice.push(array.shift());
+//       }
+//       result.push(slice);
+//   }
+//   return result;
+// }
 module.exports = chunk;
